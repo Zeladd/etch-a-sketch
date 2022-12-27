cdf5bab9e8753e8d0ba9849400b1c959a6197c01
Zeladd <filippesteam@gmail.com>
2022-12-27 19:12:28 +0200

    Finish drawing on board with black

--- logic.js
+++ logic.js
@@ -1,4 +1,10 @@
 const board = document.querySelector("#board");
+let usedColor = "black";
+let mouseDown = false;
+
+function changeColor(container) {
+    container.style.backgroundColor = usedColor;
+}
 
 function createBoard(size) {
     for (let i = 0; i < size; i++) {
@@ -8,9 +14,16 @@ function createBoard(size) {
         for (let j = 0; j < size; j++) {
             const pixel = document.createElement("div");
             pixel.className = "pixel";
+            pixel.addEventListener('mousedown', () => mouseDown = true);
+            pixel.addEventListener('mouseup', () => mouseDown = false);
+            pixel.addEventListener('mousemove', () => {
+                if (mouseDown) {
+                    changeColor(pixel);
+                }
+            });
             row.appendChild(pixel);
         }
     }
 }
 
-createBoard(16);
+createBoard(32);
